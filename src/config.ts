d600df081a3256eab6f0f1b5c0a3790275b36560
muzabol2 <bolekbilly@gmail.com>
2024-09-05 10:19:03 +0200

    Add next-cloudinary img

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const config = {
+  title: "meme-generator",
+  githubUrl: "https://github.com/muzabol2/meme-generator",
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        githubBackground: "#0d1117",
+        githubHeader: "#161b22",
+      },
     },
   },
   plugins: [],
